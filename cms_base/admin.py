08edefe2f0e54fd2f9b02180525dc5c2bed8ecb3
okan <okan@Okan-MBP.home>
2022-06-14 22:42:52 +0300

    [CMS-BASE]: Base models for CMS implemented

--- cms_base/admin.py
+++ cms_base/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from cms_base.models import Article, Resource, Tag
 
-# Register your models here.
+class ResourcesInline(admin.TabularInline):
+    model = Resource
+    fk_name = 'owned_article'
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ResourcesInline]
+    readonly_fields=('author',)
+    list_display = ('title', 'created_at')
+    list_filter = ('tag', 'status', 'author')
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'author', None) is None:
+            obj.author = request.user
+        obj.save()
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Tag)
+
--- cms_base/models.py
+++ cms_base/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+# imported for WYSIWYG editor
+from tinymce.models import HTMLField
+
+# imported for author field in Article object
+from django.contrib.auth.models import User
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(BaseModel):
+    tag_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tag_name
+
+class Article(BaseModel):
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    header_image_src = models.URLField(max_length=300, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    content = HTMLField()
+    tag = models.ManyToManyField(Tag)
+
+    STATUS_TYPES = (
+        ('U', 'Unpublished'),
+        ('P', 'Published'),
+    )
+    status = models.CharField(max_length=1, choices=STATUS_TYPES)
+
+    def __str__(self):
+        return self.title
+
+
+class Resource(BaseModel):
+    url = models.URLField(max_length=300)
+    short_name = models.CharField(max_length=50)
+    RESOURCE_TYPES = (
+        ('A', 'Article'),
+        ('V', 'Video'),
+    )
+    resource_type = models.CharField(max_length=1, choices=RESOURCE_TYPES)
+    owned_article = models.ForeignKey(Article, related_name='resources', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.short_name

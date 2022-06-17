74b72a4b41be746d461d4982aeeb49a3519dc9ac
findokan <findokan@gmail.com>
2022-06-18 00:21:22 +0300

    [CMS-DevOps]: DjangoHeroku pip package implemented for db sync

--- white_label_cms/settings.py
+++ white_label_cms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,6 @@ REST_FRAMEWORK = {
 # Static settings
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Heroku settings
+django_heroku.settings(locals())
